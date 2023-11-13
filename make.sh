2fcb94e4d196e7280112fd66ca8c5be49ecf50b4
Bruno Duyé <brunetton@brunetton.org>
2023-11-13 14:37:13 +0100

    Fix CSS path

--- make.sh
+++ make.sh
@@ -1,5 +1,9 @@
+#!/bin/env bash
 if [ -z "$1" ]; then
     echo "Usage: <nom_du_fichier.md>"
     exit -1;
 fi
-mdpdf "$1" --style=mdpdf/style.css --border-top=25 --border-bottom=55 --border-right=45 --border-left=45
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )  # https://stackoverflow.com/a/246128/276894
+echo $SCRIPT_DIR
+# workaround for https://github.com/BlueHatbRit/mdpdf/issues/162
+mdpdf "$1" --style=$SCRIPT_DIR/style.css --border-top=25 --border-bottom=55 --border-right=45 --border-left=45
